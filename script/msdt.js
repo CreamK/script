c161765189547f2ae765f343fbe3f5f8a26bf8f7
CreamK <1787536652@qq.com>
2024-08-04 19:57:28 +0800

    update

--- script/msdt.js
+++
@@ -1,8 +0,0 @@
-// 民生答题
-let html = $response.body;
-
- html =
-  html.replace(/("otitle": "[\s\S]+?)(",[\s\S]+?"oanswer": ")(.+?)(",)/gi, '$1@$3$2$3$4');
-
-$done({ body: html });
-// https://api.myminapp.com/hserve url response-body "correct":\s?true,\s?"name":\s?" response-body "correct": true, "name": "7@
